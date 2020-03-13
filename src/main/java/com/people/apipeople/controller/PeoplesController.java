d84bb07557c139b2739bc93d0ab262b24117cfc0
Joao Paulo Felix <jpfgs@ecomp.poli.br>
2020-03-13 20:13:25 -0300

    add swagger

--- src/main/java/com/people/apipeople/controller/PeoplesController.java
+++ src/main/java/com/people/apipeople/controller/PeoplesController.java
@@ -18,40 +18,50 @@ import reactor.core.publisher.Mono;
 import com.people.apipeople.models.People;
 import com.people.apipeople.service.PeopleService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping(value="/api")
+@Api(value="API REST People")
 public class PeoplesController {
 
 	@Autowired
 	PeopleService service;
 	
 	@GetMapping(value="")
+	@ApiOperation(value="Inital page")
 	public Flux<People> index() {
 		return this.all();
 	}
 	
 	@GetMapping(value="/peoples")
+	@ApiOperation(value="Return one list of people")
 	public Flux<People> all() {
 		return service.findAll();
 	}
 	
 	@GetMapping(value="/peoples/{id}")
+	@ApiOperation(value="Return only unique people")
 	public Mono<People> find(@PathVariable String id) {
 		return service.findById(id);
 	}
 	
 	@PostMapping(value="/peoples")
+	@ApiOperation(value="Register one people")
 	public Mono<People> save(@RequestBody People people){
 		return service.save(people);
 	}
 	
 	@PutMapping("/peoples")
+	@ApiOperation(value="Update the informartions of a people")
 	public Mono<People> update(@RequestBody @Valid People people) {
 		return service.save(people);
 	}
 	
 	@DeleteMapping("/peoples")
+	@ApiOperation(value="Detele one people")
 	public Mono<Void> delete(@RequestBody @Valid People people) {
 		return service.delete(people);
 	}
